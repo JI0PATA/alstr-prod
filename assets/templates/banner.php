ed229dfe20993998143bbd70d48d5a93d317920b
JI0PATA <wanja.shukov14@gmail.com>
2018-07-02 09:30:54 +0300

    Исправлен баннер

--- assets/templates/banner.php
+++ assets/templates/banner.php
@@ -1,4 +1,4 @@
-<div class="banner banner__inverse-color">
+<div class="banner">
     <label for="callback__form_checkbox" class="banner__section banner__button-action">Закажи сейчас</label>
     <div class="banner__section banner__text">И получи скидку 35%</div>
 </div>
