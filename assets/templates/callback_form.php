3a5fedd094c7e7f9121d5f572b5d75d4bc0fd645
JI0PATA <wanja.shukov14@gmail.com>
2018-07-18 14:24:14 +0300

    Открытие формы при клике на любую часть баннера

--- assets/templates/callback_form.php
+++ assets/templates/callback_form.php
@@ -5,9 +5,9 @@
     <form action="/order_callback" class="header__callback" id="callback__form_input" method="POST">
         <h3>Оставьте свои данные и наш специалист свяжется с Вами!</h3>
         <input type="text" class="form__input" placeholder="Имя"
-               style="background-image: url('assets/img/icon_author.png')" id="callback__form_name" name="name">
+               style="background-image: url('assets/img/icon_author.png')" id="callback__form_name" name="name" required>
         <input type="number" class="form__input" placeholder="Номер телефона"
-               style="background-image: url('assets/img/icon_phone.png')" id="callback__form_call" name="call">
+               style="background-image: url('assets/img/icon_phone.png')" id="callback__form_call" name="call" required>
         <button class="form__button">отправить</button>
     </form>
 </div>
